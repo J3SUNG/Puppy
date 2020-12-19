0b45edfecf51401489e433b4bfb2ca334a5166f7
J3SUNG <wptjd6141@naver.com>
2020-12-18 17:00:34 -0800

    Introduce Puppy

---
+++ question.php
@@ -0,0 +1,16 @@
+<html>
+<body>
+
+<h1> 질문해주셔서 감사합니다.</h1>
+
+반갑습니다. <?php echo $_POST["name"]; ?>님<br>
+질문자의 전화번호는 : <?php echo $_POST["number"]; ?>입니다.<br>
+질문자의 성별은 <?php echo $_POST["gender"]; ?>성 입니다. <br>
+<br>
+질문은 <?php echo $_POST["what"]; ?>에 관한 것이며<br>
+질문의 제목은 <?php echo $_POST["title"]; ?><br>
+질문의 내용은 <?php echo $_POST["cont"]; ?>입니다.<br>
+<br>
+감사합니다.
+</body>
+</html>
